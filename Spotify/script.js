9551eaee00d7763ef0bfd485dcdd0547d88df7da
Divyam Chauhan <mi.divyam@gmail.com>
2023-05-29 19:37:57 +0530

    ProgressBar_Updated

--- Spotify/script.js
+++ Spotify/script.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio("1.mp3");
 let masterPlay = document.getElementById("MasterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 
 let songs = [
   {
@@ -41,13 +42,29 @@ let songs = [
 //Handle Play Pause Button
 masterPlay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
-    console.log("played");
+    // console.log("played");
     audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");  
+    gif.style.opacity = 1;
+  }
+  else{
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
   }
 });
 
-myProgressBar.addEventListener("timeupdate", () => {
+audioElement.addEventListener("timeupdate", () => {
   console.log("timeupdate");
+  let progress = parseInt(audioElement.currentTime/audioElement.duration * 100 );
+  console.log(progress);
+  myProgressBar.value = progress ; 
 });
+
+myProgressBar.addEventListener("change",()=>{
+  audioElement.currentTime = myProgressBar.value * audioElement.duration / 100 ;
+})
 
 // audioElement.play();
