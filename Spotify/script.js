704e9ec3920227edce4363592f201422638e42a4
Divyam Chauhan <mi.divyam@gmail.com>
2023-05-30 20:33:23 +0530

    update

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,10 +1,11 @@
 console.log("Welcome to my App");
 
 let songIndex = 0;
-let audioElement = new Audio("1.mp3");
+let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("MasterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
   {
@@ -14,30 +15,36 @@ let songs = [
   },
   {
     songName: "Get Ready To Fight",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
   },
   {
     songName: "Get Ready To Fight",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
   },
   {
     songName: "Get Ready To Fight",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
   },
   {
     songName: "Get Ready To Fight",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
   },
   {
     songName: "Get Ready To Fight",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    filePath: "songs/6.mp3",
+    coverPath: "covers/6.jpg",
   },
 ];
+
+songItems.forEach((element, i) => {
+  // console.log(element, i);
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
 //Handle Play Pause Button
 masterPlay.addEventListener("click", () => {
@@ -45,10 +52,9 @@ masterPlay.addEventListener("click", () => {
     // console.log("played");
     audioElement.play();
     masterPlay.classList.remove("fa-play-circle");
-    masterPlay.classList.add("fa-pause-circle");  
+    masterPlay.classList.add("fa-pause-circle");
     gif.style.opacity = 1;
-  }
-  else{
+  } else {
     audioElement.pause();
     masterPlay.classList.remove("fa-pause-circle");
     masterPlay.classList.add("fa-play-circle");
@@ -57,14 +63,37 @@ masterPlay.addEventListener("click", () => {
 });
 
 audioElement.addEventListener("timeupdate", () => {
-  console.log("timeupdate");
-  let progress = parseInt(audioElement.currentTime/audioElement.duration * 100 );
-  console.log(progress);
-  myProgressBar.value = progress ; 
+  // console.log("timeupdate");
+  let progress = parseInt(
+    (audioElement.currentTime / audioElement.duration) * 100
+  );
+  // console.log(progress);
+  myProgressBar.value = progress;
 });
 
-myProgressBar.addEventListener("change",()=>{
-  audioElement.currentTime = myProgressBar.value * audioElement.duration / 100 ;
-})
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
 
-// audioElement.play();
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = "songs/1.mp3";
+      audioElement.currentTime = 0;
+      audioElement.play();
+    });
+  }
+);
